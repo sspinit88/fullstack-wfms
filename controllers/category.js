3acfcd0da3928429462cfe66fa143816d2793e22
Sergey Pashnin <s.pashnin@encry.ru>
2019-01-21 14:32:01 +0300

    get and delete categories

--- controllers/category.js
+++ controllers/category.js
@@ -1,19 +1,56 @@
-module.exports.getAll = function(req, res) {
-  res.json({message: 'Categories'});
+const Category = require('../models/Category');
+const errorHandler = require('../utils/errorHandler');
+const Position = require('../models/Position');
+
+
+exports.getAll = async function (req, res) {
+  try {
+    const categories = await Category.find({
+      user: req.user.id,
+    });
+    res.status(200).json(categories);
+  } catch (e) {
+    errorHandler(res, e);
+  }
 };
 
-module.exports.getById = function(req, res) {
-
+module.exports.getById = async function (req, res) {
+  try {
+    const category = await Category.findById(req.params.id);
+    res.status(200).json(category);
+  } catch (e) {
+    errorHandler(res, e);
+  }
 };
 
-module.exports.remove = function(req, res) {
-
+module.exports.remove = async function (req, res) {
+  try {
+    await Category.remove({
+      id: req.params.id
+    });
+    await Position.remowe({
+      category: req.params.id,
+    });
+    res.status(200).json({
+      message: 'Категория удалена.',
+    });
+  } catch (e) {
+    errorHandler(res, e);
+  }
 };
 
-module.exports.create = function(req, res) {
+module.exports.create = async function (req, res) {
+  try {
 
+  } catch (e) {
+    errorHandler(res, e);
+  }
 };
 
-module.exports.update = function(req, res) {
+module.exports.update = async function (req, res) {
+  try {
 
+  } catch (e) {
+    errorHandler(res, e);
+  }
 };
